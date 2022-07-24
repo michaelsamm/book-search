6abb86b0480fb22ae559176d4a185c9dbfc8f38f
msamm <msamm93@gmail.com>
2022-07-24 15:27:45 -0500

    query and mutation resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,47 @@ const { gql } = require('apollo-server-express');
 
 // Create typeDefs
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String @id
+        image: String
+        link: String
+        title: String
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String
+        bookId: String @id
+        image: String
+        link: String
+        title: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: String!): User
+    }
 `
 
 // export the typeDefs
